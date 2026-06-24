fe7559a02e667fd0e0b1a608532640588699304b
Md Zeeshan <zeeshan.codev@gmail.com>
2026-06-24 16:14:01 +0530

    h

---
+++ frontend/src/components/home/CatogoriesCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CatogoriesCard = () => {
+  return (
+    <div>CatogoriesCard</div>
+  )
+}
+
+export default CatogoriesCard
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/home/Hero";
 import FeaturedCourses from "../components/home/FeaturedCourses";
+import Categories from "../components/home/Categories";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedCourses />
+      <Categories />
     </>
   );
 };
