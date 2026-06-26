cd1b945e185114374d314f7e9d554860ef822deb
Md Zeeshan <zeeshan.codev@gmail.com>
2026-06-26 15:34:04 +0530

    :h

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import FeaturedCourses from "../components/home/FeaturedCourses";
 import Categories from "../components/home/Categories";
 import TopInstructors from "../components/home/TopInstructors";
 import Testimonials from "../components/home/Testimonials";
+import CTA from "../components/home/CTA";
 import Footer from "../components/common/Footer";
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
       <Categories />
       <TopInstructors />
       <Testimonials />
+       <CTA />
       <Footer />
     </>
   );
