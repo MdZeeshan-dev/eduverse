34da8e83ce359ca80bddaaee651471c728559ac9
Md Zeeshan <zeeshan.codev@gmail.com>
2026-07-01 23:01:24 +0530

    j

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -27,9 +27,7 @@ const Navbar = ({ authPage = false }) => {
               EduVerse
             </h1>
 
-            <p className="-mt-1 text-xs text-gray-500">
-              Learn Without Limits
-            </p>
+            <p className="-mt-1 text-xs text-gray-500">Learn Without Limits</p>
           </div>
         </NavLink>
 
