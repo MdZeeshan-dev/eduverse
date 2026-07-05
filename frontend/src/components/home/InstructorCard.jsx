01a49ce5f623246a4255839b453606f2c2bf2b03
Md Zeeshan <zeeshan.codev@gmail.com>
2026-07-05 22:49:42 +0530

    h

--- frontend/src/components/home/InstructorCard.jsx
+++ frontend/src/components/home/InstructorCard.jsx
@@ -25,7 +25,6 @@ const InstructorCard = ({ instructor }) => {
         </div>
 
         <div className="flex items-center gap-1">
-          
           <FaUsers className="text-blue-600" />
           <span>{(students / 1000).toFixed(1)}k</span>
         </div>
