d4af538247313d1e157e55ae356e5548501db8d5
Md Zeeshan <zeeshan.codev@gmail.com>
2026-08-15 23:03:37 +0530

    h

--- Frontend/src/components/Carousel.jsx
+++ Frontend/src/components/Carousel.jsx
@@ -14,6 +14,9 @@ const Carousel = () => {
 
   // Create a loop by adding first and last images as clones
   const slides = [
+
+
+    
     images[totalSlides - 1], // Clone last image at the beginning
     ...images,
     images[0], // Clone first image at the end
