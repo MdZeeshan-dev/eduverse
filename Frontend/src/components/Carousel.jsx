4aeef8aea93c2233df288771635e4e0208694eaf
Md Zeeshan <zeeshan.codev@gmail.com>
2026-08-15 23:06:57 +0530

    h

--- Frontend/src/components/Carousel.jsx
+++ Frontend/src/components/Carousel.jsx
@@ -16,13 +16,14 @@ const Carousel = () => {
   const slides = [
 
 
+
+
     images[totalSlides - 1], // Clone last image at the beginning
 
 
 
     ...images,
     images[0], // Clone first image at the end
-    
   ];
 
   const prevSlide = () => {
