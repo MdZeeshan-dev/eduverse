60e48e5900aeb8901b1be30d85cbdfa321d40ca5
Md Zeeshan <zeeshan.codev@gmail.com>
2026-08-20 13:14:07 +0530

    h

--- Frontend/src/redux/authSlice.js
+++ Frontend/src/redux/authSlice.js
@@ -128,6 +128,8 @@ const authSlice = createSlice({
       .addCase(changePassword.fulfilled, (state, action) => {
         state.loading = false;
         state.success = true;
+
+        
       })
       .addCase(changePassword.rejected, (state, action) => {
         state.loading = false;
