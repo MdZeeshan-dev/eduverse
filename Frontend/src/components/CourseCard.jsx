9bf10792a40e951d23d2eee5abcfbdd4b917a42b
Md Zeeshan <zeeshan.codev@gmail.com>
2026-09-02 23:12:04 +0530

    h

--- Frontend/src/components/CourseCard.jsx
+++ Frontend/src/components/CourseCard.jsx
@@ -7,4 +7,32 @@ function CourseCard({ image, category, heading, level, duration, link }) {
     <Link to={link} className="group">
       <div className="bg-white min-h-80 shadow-lg overflow-hidden transition-all group-hover:shadow-2xl border-[1px] border-gray-300 p-4 group-hover:scale-105">
         {/* Course Image */}
-        <div className="w-full overflow-h
+        <div className="w-full overflow-hidden">
+          <img
+            src={image}
+            alt="Course"
+            className="w-[300px] h-[150px] object-cover transform group-hover:scale-105 transition-all"
+          />
+        </div>
+
+        <div className="mt-4 space-y-2 flex flex-col">
+          <span className="text-sm font-semibold uppercase text-blue-500">
+            {category}
+          </span>
+
+          <h2 className="text-lg font-bold text-gray-900 h-16">{heading}</h2>
+
+          <div className="flex items-center justify-between text-gray-600 text-sm justify-self-end">
+            <span className="flex items-center">🏅 {level}</span>
+            <span className="flex items-center">
+              <FaClock className="mr-1" />
+              Duration: {duration}hrs
+            </span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default CourseCard;
