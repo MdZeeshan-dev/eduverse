f472c8412455210272fe9f978da8312e9f7c1f72
Md Zeeshan <zeeshan.codev@gmail.com>
2026-09-04 22:01:40 +0530

    g

--- Frontend/src/components/CourseCard.jsx
+++ Frontend/src/components/CourseCard.jsx
@@ -7,7 +7,13 @@ function CourseCard({ image, category, heading, level, duration, link }) {
     <Link to={link} className="group">
       <div className="bg-white min-h-80 shadow-lg overflow-hidden transition-all group-hover:shadow-2xl border-[1px] border-gray-300 p-4 group-hover:scale-105">
         {/* Course Image */}
-     
+        <div className="w-full overflow-hidden">
+          <img
+            src={image}
+            alt="Course"
+            className="w-[300px] h-[150px] object-cover transform group-hover:scale-105 transition-all"
+          />
+        </div>
 
         <div className="mt-4 space-y-2 flex flex-col">
           <span className="text-sm font-semibold uppercase text-blue-500">
